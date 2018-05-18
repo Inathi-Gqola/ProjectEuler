a97e0224a83a439963f3aefd1c98db67cac2b193
Inathi Gqola <inathi.gqola@outlook.com>
2018-05-18 20:16:23 +0200

    Added an interface for the to bind all the problems to return a solution.

---
+++ Challenges/IProblem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Euler.Challenges
+{
+    public interface IProblem
+    {
+        object GetSoulution();
+    }
+}
